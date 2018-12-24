8c3158ef538e2ff9bea9086f33e11ac8ebc7bce8
Pavel Tereschenko <me@stupidcat.wtf>
2018-12-24 19:51:06 +0500

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# es-observable
+
+[![Build Status][status-img]][status-url]
+[![Greenkeeper badge](https://badges.greenkeeper.io/bigslycat/es-observable.svg)](https://greenkeeper.io/)
+
+[status-url]: https://travis-ci.org/bigslycat/es-observable
+[status-img]: https://travis-ci.org/bigslycat/es-observable.svg?branch=master
---
+++ rollup.config.js
@@ -0,0 +1,73 @@
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import flowEntry from 'rollup-plugin-flow-entry'
+import clear from 'rollup-plugin-clear'
+
+import packageJson from './package.json'
+
+const { dependencies } = packageJson
+
+const deps = dependencies && Object.keys(dependencies).join('|')
+const reg = deps && new RegExp(`^(${deps})($|/)`)
+
+const banner = `
+/**
+ * ${packageJson.name} v${packageJson.version}
+ * ${packageJson.description}
+ */
+`
+
+const external = id => !!reg && reg.test(id)
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'lib/index.js',
+      format: 'cjs',
+      banner,
+    },
+    plugins: [
+      clear({
+        targets: ['lib'],
+      }),
+      flowEntry(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      commonjs(),
+    ],
+    treeshake: true,
+    external,
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'esm/index.mjs',
+      format: 'esm',
+      banner,
+    },
+    plugins: [
+      clear({
+        targets: ['esm'],
+      }),
+      flowEntry(),
+      babel({
+        exclude: 'node_modules/**',
+        presets: [
+          [
+            '@babel/env',
+            {
+              targets: { node: 10 },
+              useBuiltIns: 'usage',
+              modules: false,
+            },
+          ],
+        ],
+      }),
+      commonjs(),
+    ],
+    treeshake: true,
+    external,
+  },
+]
